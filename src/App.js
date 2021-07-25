cd163cc4ed20ac7bd270f21f599ccfb616693d19
TrongVy <trongvyuit@gmail.com>
2021-07-25 16:46:59 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Cart from './Component/CartNangCao/Cart';
+// import Glasses from './Component/Glasses/Glasses';
+
+function App() {
+  return (
+    <div className="App">
+
+        {/* <Glasses/> */}
+        <Cart/>
+    </div>
+  );
+}
+
+export default App;
